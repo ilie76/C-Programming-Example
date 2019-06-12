665632dc89df6b800748ea9775b8d6e3f9446aeb
ilie76 <ilie76@gmail.com>
2019-06-12 20:41:12 +0300

    z30ex1

---
+++ z30ex1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+// version control
+//git - GitHub
+//github.com
+//bitbucket.org
+//svn
+
+
+//UML
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
